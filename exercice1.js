2928467f065f989fec73784299554ec89f882f5c
vince <vincent.harpin@orange.fr>
2022-11-22 14:35:02 +0100

    exercice 71

--- exercice1.js
+++ exercice1.js
@@ -1,8 +1,19 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
 
 document.getElementById('viewport').appendChild(monDiv);
 
+monDiv.innerText = "Ce div a été ajouté en utilisant javascript";
 
+let pomme = document.createElement('div');
+
+document.getElementById('viewport').appendChild(pomme);
+
+pomme.style.backgroundColor = 'red';
+pomme.style.width = '100px';
+pomme.style.height = '100px';
+
+let supprime = document.querySelector('span');
+supprime.remove();
 
 
 
